d33f5bd211ae909c2c897c76c3137d92099e2141
Kris' Big Boy <people12337@gmail.com>
2021-11-24 15:36:38 +0700

    fix format

--- src/webapp/pages/main/dashboard/DashBoard.tsx
+++ src/webapp/pages/main/dashboard/DashBoard.tsx
@@ -56,7 +56,7 @@ export default function DashBoard() {
     return (
         <Container style={{ width: '100%', backgroundColor: '#FFFFFF', maxWidth: '90%', height:'100%', maxHeight:'100%'}} >
             <Sick>
-            <Grid container item direction="row" xs={12} pt={3}>
+            <Grid container item direction="row" xs={11} pt={3}>
                 <ThemeProvider theme={header_name}>
                 <Grid item id="title">
                     <Typography>User</Typography>
@@ -64,7 +64,7 @@ export default function DashBoard() {
                 </ThemeProvider>
                 <ThemeProvider theme={icon_name}>
                 <Grid item container id="actionzone" justifyContent='space-between' py={1}>
-                    <Grid item container justifyContent='center' xs={2} px={3}>
+                    <Grid item container justifyContent='center' xs={1} px={3}>
                        <Typography >All Meter</Typography> 
                        <Img src="/assets/icon/Member.png"/>
                        <Number>
@@ -82,7 +82,7 @@ export default function DashBoard() {
                        <Typography >Today New Registered User</Typography> 
                        <Img src="/assets/icon/New-Member.png"/>
                        <Number>
-                            <Typography style={{fontSize:26}} px={2}>27</Typography>
+                            <Typography style={{fontSize:26}} px={6}>27</Typography>
                        </Number>
                     </Grid>
 
@@ -134,32 +134,32 @@ export default function DashBoard() {
                 <ThemeProvider theme={icon_name}>
                 <Grid item container id="actionzone" justifyContent='space-between' py={1}>
                     
-                    <Grid item container justifyContent='center' direction='row' xs={3} px={4}>
+                    <Grid item container justifyContent='center' direction='row' xs={2} px={4}>
                        <Typography>Total PV Generate</Typography>
                        <Img src="/assets/icon/311.png"/>
                        <Number>
                             <Typography style={{fontSize:26}} px={7}>27</Typography>
                        </Number>
                     </Grid>
-                    <Grid item container justifyContent='center' direction='row' xs={3}>
+                    <Grid item container justifyContent='center' direction='row' xs={2}>
                        <Typography>Total Energy Storage Charge/Discharge</Typography>  
                        <Img src="/assets/icon/310.png"/>
                        <Number>
-                            <Typography style={{fontSize:26}} px={10}>27</Typography>
+                            <Typography style={{fontSize:26}} px={11}>27</Typography>
                        </Number>
                     </Grid>
-                    <Grid item container justifyContent='center' direction='row' xs={2}>
-                       <Typography>Total Grid Used</Typography>
+                    <Grid item container justifyContent='center' direction='row' xs={3}>
+                       <Typography px={12}>Total Grid Used</Typography>
                        <Img src="/assets/icon/312.png"/>
                        <Number>
-                            <Typography style={{fontSize:26}} px={7}>27</Typography>
+                            <Typography style={{fontSize:26}} px={20}>27</Typography>
                        </Number>
                     </Grid>
                     <Grid item container justifyContent='center' direction='row' xs={2}>
-                       <Typography>Total Energy Load</Typography>
+                       <Typography px={6}>Total Energy Load</Typography>
                        <Img src="/assets/icon/313.png"/>
                        <Number>
-                            <Typography style={{fontSize:26}} px={7}>27</Typography>
+                            <Typography style={{fontSize:26}} px={20}>27</Typography>
                        </Number>
                     </Grid>
                     
@@ -200,7 +200,7 @@ export default function DashBoard() {
                        <Typography >Total Energy Sales (Net)</Typography>
                        <Img src="/assets/icon/321.png"/>
                        <Number>
-                            <Typography style={{fontSize:26}} px={2}>27</Typography>
+                            <Typography style={{fontSize:26}} px={2} py={3}>27</Typography>
                        </Number>
                     </Grid>
                     <Grid item direction='row' justifyContent='center' xs={3} style={{textAlign:'center'}}>
@@ -214,7 +214,7 @@ export default function DashBoard() {
                        <Typography >Total Energy Imbalance (Net)</Typography>
                        <Img src="/assets/icon/323.png"/>
                        <Number>
-                            <Typography style={{fontSize:26}} px={2}>27</Typography>
+                            <Typography style={{fontSize:26}} px={2} py={2}>27</Typography>
                        </Number>
                     </Grid>
                     
@@ -234,28 +234,28 @@ export default function DashBoard() {
                        <Typography >Total Energy Trading (Net)</Typography>
                        <Img src="/assets/icon/318.png"/>
                        <Number>
-                            <Typography style={{fontSize:26}} px={7}>27</Typography>
+                            <Typography style={{fontSize:26}} px={12}>27</Typography>
                        </Number>
                     </Grid>
                     <Grid item container direction='row' justifyContent='center' xs={2}>
-                       <Typography >Total Grid Used</Typography>
+                       <Typography px={5} >Total Grid Used</Typography>
                        <Img src="/assets/icon/317.png"/>
-                       <Number>
-                            <Typography style={{fontSize:26}} px={7}>27</Typography>
-                       </Number>
-                    </Grid>
-                    <Grid item container direction='row' justifyContent='center' xs={3} px={1}>
-                       <Typography >Total Wheeling Charge</Typography>
-                       <Img src="/assets/icon/294.png"/>
                        <Number>
                             <Typography style={{fontSize:26}} px={10}>27</Typography>
                        </Number>
                     </Grid>
+                    <Grid item container direction='row' justifyContent='center' xs={3} px={1}>
+                       <Typography px={7} >Total Wheeling Charge</Typography>
+                       <Img src="/assets/icon/294.png"/>
+                       <Number>
+                            <Typography style={{fontSize:26}} px={20}>27</Typography>
+                       </Number>
+                    </Grid>
                     <Grid item container direction='row' justifyContent='center' xs={1}>
-                       <Typography >Acc. REC</Typography>
+                       <Typography px={2} >Acc. REC</Typography>
                        <Img src="/assets/icon/319.png"/>
                        <Number>
-                            <Typography style={{fontSize:26}} px={2}>27</Typography>
+                            <Typography style={{fontSize:26}} px={10}>27</Typography>
                        </Number>
                     </Grid>
                     <Grid item justifyContent="flex-end">
