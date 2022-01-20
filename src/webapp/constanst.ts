ba331848ce694c498853846aeb41e8746436e262
A1theend0ftime <time2242@gmail.com>
2022-01-20 14:18:07 +0700

    New management
    lastest New management

--- src/webapp/constanst.ts
+++ src/webapp/constanst.ts
@@ -1,3 +1,4 @@
 export const userApi = 'https://egat-p2p-webadmin-profile.di.iknowplus.co.th';
 export const refApi = 'https://egat-p2p-webadmin-ref-data.di.iknowplus.co.th';
-export const newsApi = 'https://egat-p2p-api.di.iknowplus.co.th';//test
+// export const newsApi = 'https://egat-p2p-api.di.iknowplus.co.th';//test
+export const newsApi = 'https://ercapip2p.egat.co.th';
